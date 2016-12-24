01a58abdade0eb24c4a007c330c4dd3a342fdd28
Liam B <onevoltten@googlemail.com>
2016-12-24 12:55:23 +0900

    Removed quality option
    Fixed syntax error
    Get resolution from video page

--- KissDownloader.py
+++ KissDownloader.py
@@ -14,15 +14,15 @@ except ImportError:
 # TODO simultaneous downloads - KissDownloadManager.py WIP
 # TODO handling for episodes with multiple values e.g. 116-117
 
-website = "kissanime.ru"
+website = "kissanime.ru" # ["kissanime.ru", "kisscartoon.me", "kissasian.com"]
 user_name = "" # required
 user_password = "" # required
-destination = "" # required
+destination = "" # required (folder to download files)
 destinationx = destination # remove in next update
 download_limit = "40" # episode count to retrieve before download; recommended less than 50
 retrieve_last = "20" # rechecks if file exists, used because kissanime site is so unreliable
 episode_min = "0" # first episode to download
-move_mp4 = "0" # set to 1 to move all *.mp4 files to up directory on download complete
+move_mp4 = "1" # set to 1 to move all *.mp4 files to up directory on download complete
 episode_current = "0" # do not change
 prefix = "" # not required
 
@@ -161,7 +161,7 @@ class KissDownloader:
                         return ["http://" + site + "" + currentlink.lower(), False]
         return ["", False]
 
-    def get_video_src(self, episode_page, qual):
+    def get_video_src(self, episode_page):
         # parses the video source link from the streaming page, currently chooses the highest available quality
 
         x = True
@@ -190,19 +190,22 @@ class KissDownloader:
         #for link in soup.findAll('a', string="1920x1080.mp4"):
         #    return [link.get('href'), ".mp4"]
         for link in soup.findAll('a', string="1280x720.mp4"):
-            return [link.get('href'), ".mp4"]
+            return [link.get('href'), ".mp4", "720p"]
         for link in soup.findAll('a', string="960x720.mp4"):
-            return [link.get('href'), ".mp4"]
+            return [link.get('href'), ".mp4", "720p"]
         for link in soup.findAll('a', string="854x480.mp4"):
-            return [link.get('href'), ".mp4"]
+            return [link.get('href'), ".mp4", "480p"]
         for link in soup.findAll('a', string="480x360.mp4"):
-            return [link.get('href'), ".mp4"]
+            return [link.get('href'), ".mp4", "480p"]
         for link in soup.findAll('a', string="640x360.mp4"):
-            return [link.get('href'), ".mp4"]
-        for link in soup.findAll('a', string="320x180.3gp"):
-            return [link.get('href'), ".3pg"]
+            return [link.get('href'), ".mp4", "360p"]
+        '''
+        # uncomment if you want these resolutions (really low quality)
         for link in soup.findAll('a', string="320x240.3pg"):
-            return [link.get('href'), ".3pg"]
+            return [link.get('href'), ".3pg", "240p"]
+        for link in soup.findAll('a', string="320x180.3gp"):
+            return [link.get('href'), ".3pg", "180p"]
+        '''
         return ["false", ""]
 
     def download_video(self, url, name, destination, episode):
@@ -271,17 +274,16 @@ class KissDownloader:
             else:
                 print(str(epcount)+" is not int!")
         
-        if(epcount > retrieve_last):
-            epcount = epcount - retrieve_last
+        if(int(epcount) > int(retrieve_last)):
+            epcount = int(epcount) - int(retrieve_last)
         #else:
         #    print("No downloaded files found")
 
-        #p = [user, password, title, anime, season, episode_min, episode_max, destination, quality, site]
         # takes a list of parameters and uses them to download the show
-        l = self.login(p[0], p[1], p[9])  # 0 are the indices of the username and password from get_params()
+        l = self.login(p[0], p[1], p[8])  # 0 are the indices of the username and password from get_params()
         while not l:
             print("Login failed, try again")
-            l = self.login(p[0], p[1], p[9])
+            l = self.login(p[0], p[1], p[8])
 
         time.sleep(1)
         self.rootPage = self.scraper.get(p[3]).content  # 3 is the index of the url
@@ -290,22 +292,13 @@ class KissDownloader:
         print("Retrieve from episode " + str(epcount))
         for e in self.frange(float(epcount), int(p[6])+1, 0.5):  # 5 and 6 are episodes min and max
             if(ecount < int(download_limit)):
-                page = self.get_episode_page(e, p[9])
+                page = self.get_episode_page(e, p[8])
                 # page = [page_url, isUncensored]
                 if page[0] == "":
                     pass
                 else:
-                    video = self.get_video_src(page[0], p[8]) #8 is the quality
-                    if p[8] in ["1920x1080.mp4", "1280x720.mp4", "960x720.mp4"]:
-                        resolution = "720p"
-                    elif p[8] in ["480x360.mp4", "320x180.mp4"]:
-                        resolution = "480p"
-                    elif p[8] in ["640x360.mp4", "320x180.mp4", "320x240.mp4", "320x180.3gp", "320x240.3pg"]:
-                        resolution = "360p"
-                    else:
-                        resolution = p[8]
-                    # video = [url, file_extension]
-
+                    video = self.get_video_src(page[0]) #8 is the quality
+                    # video = [url, file_extension, resolution]
                     if prefix != "":
                         prefix2 = p[4] + prefix
 
@@ -313,15 +306,15 @@ class KissDownloader:
                         if page[1]:  # if episode is called uncensored
                             if e % 1 == 0:
                                 e = int(e)
-                                filename = prefix2 + p[2] + "_-_" + str(e).zfill(3) + "_" + resolution +"_BD_Kiss" + video[1]  # 2 is the title
+                                filename = prefix2 + p[2] + "_-_" + str(e).zfill(3) + "_" + video[2] +"_BD_Kiss" + video[1]  # 2 is the title
                             else:
-                                filename = prefix2 + p[2] + "_-_" + self.zpad(str(e), 3) + "_" + resolution +"_BD_Kiss" + video[1]  # 2 is the title
+                                filename = prefix2 + p[2] + "_-_" + self.zpad(str(e), 3) + "_" + video[2] +"_BD_Kiss" + video[1]  # 2 is the title
                         else:
                             if e % 1 == 0:
                                 e = int(e)
-                                filename = prefix2 + p[2] + "_-_" + str(e).zfill(3) + "_" + resolution + "_Kiss" + video[1]  # 2 is the title
+                                filename = prefix2 + p[2] + "_-_" + str(e).zfill(3) + "_" + video[2] + "_Kiss" + video[1]  # 2 is the title
                             else:
-                                filename = prefix2 + p[2] + "_-_" + self.zpad(str(e), 3) + "_" + resolution + "_Kiss" + video[1]  # 2 is the title
+                                filename = prefix2 + p[2] + "_-_" + self.zpad(str(e), 3) + "_" + video[2] + "_Kiss" + video[1]  # 2 is the title
                         print("Resolved [" + filename + "]")
                         episode_list.append((video[0], filename, p[7], str(e).zfill(3)))
                         ecount = ecount + 1
@@ -404,24 +397,24 @@ class KissDownloader:
             title = title.replace(k, v)
         
         print('Initiate... [' + title + ']')
-        return website,user_name,user_password,title,url,mal,episode_min,episode_max,destination,quality
+        return website,user_name,user_password,title,url,mal,episode_min,episode_max,destination
 
     def run_download(self):
-        # 0 website, 1 user_name,2 user_password, 3 title, 4 url, 5 mal, 6 episode_min, 7 episode_max, 8 destination, 9 quality
+        # 0 website, 1 user_name,2 user_password, 3 title, 4 url, 5 mal, 6 episode_min, 7 episode_max, 8 destination
         destination_folder = self[8].replace("\\", "/")
         if destination_folder.endswith('/'):
             destination = destination_folder + self[3] + "/"
         else:
             destination = destination_folder + "/" + self[3] + "/"
         '''destination = destination_folder + "/"'''
-        params = [self[1], self[2], self[3], self[4], self[5], self[6], self[7], destination, self[9], self[0]]
+        params = [self[1], self[2], self[3], self[4], self[5], self[6], self[7], destination, self[0]]
         #print(params)
         KissDownloader(params)
 
     def init():
-        # 0 website, 1 user_name,2 user_password, 3 title, 4 url, 5 mal, 6 episode_min, 7 episode_max, 8 destination, 9 quality
-        website,user_name,user_password,title,url,mal,episode_min,episode_max,destination,quality = KissDownloader.read_config()
-        KissDownloader.run_download([website,user_name,user_password,title,url,mal,episode_min,episode_max,destination,quality])
+        # 0 website, 1 user_name,2 user_password, 3 title, 4 url, 5 mal, 6 episode_min, 7 episode_max, 8 destination
+        website,user_name,user_password,title,url,mal,episode_min,episode_max,destination = KissDownloader.read_config()
+        KissDownloader.run_download([website,user_name,user_password,title,url,mal,episode_min,episode_max,destination])
         episodes_list = []
         for tup in episodes_list:
             url = tup[0]
@@ -430,7 +423,6 @@ class KissDownloader:
             KissDownloader.download_video(KissDownloader, url, filename, destination, episode)
 
 if __name__ == "__main__":
-    #params = [user, password, title, anime, season, episode_min, episode_max, destination, quality, site]
     KissDownloader
     KissDownloader.init()
 
