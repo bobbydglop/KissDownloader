c2307c531d5e2bb9938ae1ea93fdfe12247992c7
Liam B <Onevoltten@googlemail.com>
2017-02-14 08:03:10 +0000

    External settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+# ----  CONFIG START ---- #
+
+username = "" # Required
+userpassword = "" # Required
+destination_folder = "" # Optional
+download_threads = 8 # Asynchronous downloads count [recommended 4-16]
+
+# ----  CONFIG END ---- #
