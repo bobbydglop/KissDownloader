4e0b14d0c5d6a22805c0c635fc77bf69157bb6f5
Liam B <Onevoltten@googlemail.com>
2017-03-26 23:21:36 +1000

    Update
    improved GUI logic

--- KissDownloaderGUI.py
+++ KissDownloaderGUI.py
@@ -30,6 +30,8 @@ class OneVoltTen(tk.Tk):
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
+        if 'PageOne' in str(cont):
+            pass # run readCSV function
         frame = self.frames[cont]
         frame.tkraise()
 
@@ -47,9 +49,9 @@ class StartPage(tk.Frame):
         self.title = Entry(self, width=30)
         self.title.pack()
 
-        Label(self, text="Enter series episode count: ").pack()
-        self.episode_count = Entry(self, width=30)
-        self.episode_count.pack()
+        #Label(self, text="Enter series episode count: ").pack()
+        #self.episode_count = Entry(self, width=30)
+        #self.episode_count.pack()
 
         Label(self, text="Download from episode (min): ").pack()
         self.episode_min = Entry(self, width=30)
@@ -68,7 +70,7 @@ class StartPage(tk.Frame):
         if(demo_data == 1):
             self.url.insert(0, "http://kissanime.ru/Anime/Re-Zero-kara-Hajimeru-Isekai-Seikatsu")
             self.title.insert(0, "Re Zero")
-            self.episode_count.insert(0, "25")
+            #self.episode_count.insert(0, "25")
             self.episode_min.insert(0, "0")
             self.episode_max.insert(0, "0")
         self.quality_select.set("720p")
@@ -83,7 +85,7 @@ class StartPage(tk.Frame):
             or "http://" in str(self.url.get()) \
             or "www." in str(self.url.get())):
             valid = 0
-            for checkint in ["episode_count","episode_min","episode_max"]:
+            for checkint in ["episode_min","episode_max"]:
                 try_this = "self." + checkint + ".get()"
                 if not eval(try_this).isdigit():
                     print(checkint + " must be number!")
@@ -92,25 +94,32 @@ class StartPage(tk.Frame):
                 with open( dir_path + '/resolved.csv', 'a') as csvfile:
                     thewriter = csv.writer(csvfile, delimiter=',')
                     #print(str(params))
-                    params = [str(self.title.get()), str(self.url.get()), '0', str(self.episode_count.get()), str(self.episode_min.get()), str(self.episode_max.get()), str(self.quality_select.get()[:-1])]
+                    params = [str(self.title.get()), str(self.url.get()), '0', '9999', str(self.episode_min.get()), str(self.episode_max.get()), str(self.quality_select.get()[:-1])]
                     thewriter.writerow(params)
                 #print("Queued", self.title.get())
         else:
             print('URL invalid')
 
 class PageOne(tk.Frame):
+
+    # TODO add list with scrollbar for queued items
+
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        button = tk.Button(self, text="Read queue", command=self.readCSV) # TODO on change to PageOne run function
-        button.pack()
+        self.back = tk.Button(self, text="Back", command=self.btnRestart)
+        self.back.pack()
+        self.readcsvbtn = tk.Button(self, text="Read queue", command=self.readCSV)
+        self.readcsvbtn.pack()
         self.controller=controller
         self.dir_path = dir_path=os.path.dirname(os.path.realpath(__file__))
 
     def readCSV(self):
+        self.readcsvbtn.destroy()# destory read queue button
         valid = 0
         columns = defaultdict(list) # each value in each column is appended to a list
         with open('resolved.csv','r') as f:
           reader = csv.reader(f,delimiter=',')
+          message = 0
           for row in reader:
             try:
                 if row[0]:
@@ -119,6 +128,10 @@ class PageOne(tk.Frame):
                         valid = 1
                     new_btn = Button(self, text=row[0], command=self.btnClick(row[1]))
                     new_btn.pack()
+                    message += 1
+                    if message > 5:
+                        Label(self, text="Additional queued items hidden.").pack()
+                        break
             except IndexError as e:
                 pass
 
@@ -132,6 +145,11 @@ class PageOne(tk.Frame):
         # print(row_url)
         # csv where row_url in row, delete row
         pass
+
+    def btnRestart(self):
+        self.controller.destroy()
+        app = OneVoltTen()
+        app.mainloop()
 
     def initiate(self):
         self.controller.destroy()
